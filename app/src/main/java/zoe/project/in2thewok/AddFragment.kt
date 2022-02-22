ed0523794c2b29c5f42a20b0e9d628ddcfe3561b
Zoe <zoe.pearson.2018@uni.strath.ac.uk>
2022-02-22 16:09:08 +0000

    adding a photo to the post bug fixed, can also change the photo

--- app/src/main/java/zoe/project/in2thewok/AddFragment.kt
+++ app/src/main/java/zoe/project/in2thewok/AddFragment.kt
@@ -39,8 +39,8 @@ lateinit var auth: FirebaseAuth
  */
 class AddFragment : Fragment() {
     // TODO: Rename and change types of parameters
-//    private var param1: String? = null
-//    private var param2: String? = null
+    private var param1: String? = null
+    private var param2: String? = null
     private lateinit var auth: FirebaseAuth
     private var imageUri : Uri? = null
     private var _binding: FragmentAddBinding? = null
@@ -49,13 +49,13 @@ class AddFragment : Fragment() {
     private val postCollectionRef = Firebase.firestore.collection("posts")
     private var storageRef = FirebaseStorage.getInstance().reference
     private var remoteUri: String? = null
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        arguments?.let {
-//            param1 = it.getString(ARG_PARAM1)
-//            param2 = it.getString(ARG_PARAM2)
-//        }
-//    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -63,6 +63,45 @@ class AddFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentAddBinding.inflate(inflater, container, false)
+
+        if (!remoteUri.equals(null)) {
+            Picasso.get()
+                .load(remoteUri)
+                .into(binding.imgUpload)
+            binding.tvUploadPhoto.text = "Change Photo"
+        }
+
+        binding.btnUploadData.setOnClickListener{
+            val post = Post(auth.currentUser?.uid.toString(), auth.currentUser?.displayName.toString(),
+                remoteUri, null, binding.recipeTitle.text.toString(), listOf(), binding.cuisineType.text.toString(), listOf(), binding.recipeStory.text.toString())
+            addPost(post)
+        }
+
+        val getContent = registerForActivityResult(ActivityResultContracts.GetContent()){
+                uri: Uri? ->
+            imageUri = uri
+            val user = auth.currentUser
+            val re = Regex("[^A-Za-z0-9 ]")
+            var uriLastSeg = imageUri!!.lastPathSegment
+            uriLastSeg = uriLastSeg?.let { it1 -> re.replace(it1, "") }
+            val imageRef = storageRef.child("images/" + user?.uid + "/" + uriLastSeg)
+            val upload = imageRef.putFile(imageUri!!)
+            upload.addOnSuccessListener {
+                val downloadUrl = imageRef.downloadUrl
+                downloadUrl.addOnSuccessListener {
+                    remoteUri = it.toString()
+                }
+            }
+            binding.imgUpload.setImageURI(imageUri)
+            binding.tvUploadPhoto.text = "Change Photo"
+        }
+
+        binding.cvAddImg.setOnClickListener {
+            getContent.launch("image/*")
+        }
+
+
+
         return binding.root
 
     }
@@ -71,13 +110,7 @@ class AddFragment : Fragment() {
         super.onStart()
         auth = Firebase.auth
         val db = Firebase.firestore
-        if (!remoteUri.equals(null)) {
-            Picasso.get()
-                .load(imageUri)
-                .into(binding.imgUpload)
-            binding.imgUpload.visibility = VISIBLE
-            binding.cvAddImg.visibility = INVISIBLE
-        }
+
 //        else {
 //            binding.cvAddImg.visibility = VISIBLE
 //        }
@@ -97,51 +130,48 @@ class AddFragment : Fragment() {
 //            }
 //
 //        }
-        binding.btnUploadData.setOnClickListener{
-            val post = Post(auth.currentUser?.uid.toString(), auth.currentUser?.displayName.toString(),
-                remoteUri, null, binding.recipeTitle.text.toString(), listOf(), binding.cuisineType.text.toString(), listOf(), binding.recipeStory.text.toString())
-            addPost(post)
-        }
-        binding.cvAddImg.setOnClickListener {
-            selectImage()
-        }
+
+
     }
-    private fun selectImage() {
-            val intent = Intent()
-            intent.type = "image/*"
-            intent.action = Intent.ACTION_GET_CONTENT
-            binding.imgUpload.visibility = VISIBLE
-            binding.cvAddImg.visibility = INVISIBLE
-            getResult.launch(intent)
+//    private fun selectImage() {
+//        val intent = Intent()
+//        intent.type = "image/*"
+//        intent.action = Intent.ACTION_GET_CONTENT
+//        getContent.launch("image/*")
+
+//            getResult.launch(intent)
 //        startActivityForResult(intent, 100)
-        }
-// TODO: Check if regex is necessary here, also don't upload to Firebase before the post is done
-    private val getResult =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { it ->
-            if(it.resultCode == RESULT_OK){
-                imageUri = it.data?.data!!
-                var user = auth.currentUser
-                val re = Regex("[^A-Za-z0-9 ]")
-                var uriLastSeg = imageUri!!.lastPathSegment
-                uriLastSeg = uriLastSeg?.let { it1 -> re.replace(it1, "") }
-                val imageRef = storageRef.child("images/" + user?.uid + "/" + uriLastSeg)
-                val upload = imageRef.putFile(imageUri!!)
-                upload.addOnSuccessListener {
-                    val downloadUrl = imageRef.downloadUrl
-                    downloadUrl.addOnSuccessListener {
-                        remoteUri = it.toString()
-                    }
-                }
-                binding.imgUpload.setImageURI(imageUri)
-                binding.cvAddImg.visibility = INVISIBLE
-                binding.imgUpload.visibility = VISIBLE
-            }
-        }
+//        }
+    // TODO: Check if regex is necessary here, also don't upload to Firebase before the post is done
+//    private
+
+//    private val getResult =
+//        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { it ->
+//            if(it.resultCode == RESULT_OK){
+//                imageUri = it.data?.data!!
+//                var user = auth.currentUser
+//                val re = Regex("[^A-Za-z0-9 ]")
+//                var uriLastSeg = imageUri!!.lastPathSegment
+//                uriLastSeg = uriLastSeg?.let { it1 -> re.replace(it1, "") }
+//                val imageRef = storageRef.child("images/" + user?.uid + "/" + uriLastSeg)
+//                val upload = imageRef.putFile(imageUri!!)
+//                upload.addOnSuccessListener {
+//                    val downloadUrl = imageRef.downloadUrl
+//                    downloadUrl.addOnSuccessListener {
+//                        remoteUri = it.toString()
+//                    }
+//                }
+//                binding.imgUpload.setImageURI(imageUri)
+//                binding.cvAddImg.visibility = INVISIBLE
+//                binding.imgUpload.visibility = VISIBLE
+//            }
+//        }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+    // TODO: don't make blank posts, setting the imageuri to null is enough to get rid of the iv
     private fun addPost(post: Post) = CoroutineScope(Dispatchers.IO).launch{
         val context = context?.applicationContext
         try {
@@ -152,11 +182,14 @@ class AddFragment : Fragment() {
             withContext(Dispatchers.Main) {//Dispatchers.Main sends to the UI
                 Toast.makeText(context, "Successfully made post.", Toast.LENGTH_LONG).show()
                 binding.recipeTitle.text.clear()
+                binding.cuisineType.text.clear()
+                binding.recipeStory.text.clear()
                 binding.imgUpload.setImageURI(null)
+                binding.tvUploadPhoto.text = "Add A Photo"
                 binding.imgUpload.visibility = INVISIBLE
                 imageUri = null
                 remoteUri = null
-                binding.cvAddImg.visibility = VISIBLE
+//                binding.cvAddImg.visibility = VISIBLE
             }
         } catch(e: Exception){
             withContext(Dispatchers.Main) {//Dispatchers.Main sends to the UI
@@ -165,23 +198,23 @@ class AddFragment : Fragment() {
         }
     }
 
-//    companion object {
-//        /**
-//         * Use this factory method to create a new instance of
-//         * this fragment using the provided parameters.
-//         *
-//         * @param param1 Parameter 1.
-//         * @param param2 Parameter 2.
-//         * @return A new instance of fragment HomeFragment.
-//         */
-//        // TODO: Rename and change types and number of parameters
-//        @JvmStatic
-//        fun newInstance(param1: String, param2: String) =
-//            HomeFragment().apply {
-//                arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                    putString(ARG_PARAM2, param2)
-//                }
-//            }
-//    }
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment HomeFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            HomeFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
 }
--- app/src/main/java/zoe/project/in2thewok/ProfileFragment.kt
+++ app/src/main/java/zoe/project/in2thewok/ProfileFragment.kt
@@ -34,20 +34,21 @@ private const val ARG_PARAM2 = "param2"
  */
 class ProfileFragment : Fragment() {
     // TODO: Rename and change types of parameters
-//    private var param1: String? = null
-//    private var param2: String? = null
+    private var param1: String? = null
+    private var param2: String? = null
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
     private val postCollectionRef = Firebase.firestore.collection("posts")
     private val personCollectionRef = Firebase.firestore.collection("people")
 //    private val docRef: DocumentReference? = null
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        arguments?.let {
-//            param1 = it.getString(ARG_PARAM1)
-//            param2 = it.getString(ARG_PARAM2)
-//        }
-//    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -178,23 +179,23 @@ class ProfileFragment : Fragment() {
             }
         }
     }
-//    companion object {
-//        /**
-//         * Use this factory method to create a new instance of
-//         * this fragment using the provided parameters.
-//         *
-//         * @param param1 Parameter 1.
-//         * @param param2 Parameter 2.
-//         * @return A new instance of fragment HomeFragment.
-//         */
-//        // TODO: Rename and change types and number of parameters
-//        @JvmStatic
-//        fun newInstance(param1: String, param2: String) =
-//            HomeFragment().apply {
-//                arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                    putString(ARG_PARAM2, param2)
-//                }
-//            }
-//    }
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment HomeFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            HomeFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
 }
